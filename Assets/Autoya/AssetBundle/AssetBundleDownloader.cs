78bfe266b584155b468d0dc93c1e73ee59671f3f
sassembla <toru.inoue@kissaki.tv>
2016-11-30 22:13:43 +0900

    adding functions.

---
+++ Assets/Autoya/AssetBundle/AssetBundleDownloader.cs
@@ -0,0 +1,5 @@
+namespace AutoyaFramework.AssetBundle {
+    public class AssetBundleDownloader {
+        // 機能追加、Unity標準のWebRequestで取得できるクラスを増やす。HTTPへの依存をどうしようかな〜
+    }
+}
---
+++ Assets/Autoya/AssetBundle/AssetBundleReader.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace AutoyaFramework.AssetBundle {
+    public class AssetBundleReader {
+        public T LoadAssetBundleOfResource <T> (string resourceName, Action<T> loaded, Action failed) where T : UnityEngine.Object {
+            // return new T();
+            throw new Exception("");
+        }
+        
+    }
+}
