db334e5986721a41ea183493f47de0a51402d0f2
sassembla <toru.inoue@kissaki.tv>
2017-01-10 04:17:09 +0900

    update test shell.

--- run_miyamasu_tests.sh
+++ run_miyamasu_tests.sh
@@ -1,15 +1,15 @@
-UNITY_APP=/Applications/Unity5.4.2p4/Unity.app/Contents/MacOS/Unity
+UNITY_APP=/Applications/Unity5.5.0p3/Unity.app/Contents/MacOS/Unity
 echo using Unity @ ${UNITY_APP}
 
 # update defines.
-echo -define:CLOUDBUILD > ./Assets/gmcs.rsp
+# echo -define:CLOUDBUILD > ./Assets/gmcs.rsp
 
-# update date.
-DATE=`date +%Y-%m-%d:%H:%M:%S`
-echo //${DATE} > ./Assets/MiyamasuTestRunner/Editor/Timestamp.cs
+# # update date.
+# DATE=`date +%Y-%m-%d:%H:%M:%S`
+# echo //${DATE} > ./Assets/MiyamasuTestRunner/Editor/Timestamp.cs
 
-${UNITY_APP} -batchmode -projectPath $(pwd) -executeMethod Miyamasu.TestEntryPoint.Start
-# /Applications/Unity5.4.2p4/Unity.app/Contents/MacOS/Unity -batchmode -projectPath $(pwd) -executeMethod Miyamasu.CloudBuildTestEntryPoint.Test
+${UNITY_APP} -batchmode -projectPath $(pwd) -executeMethod Miyamasu.MiyamasuTestIgniter.RunBatchMode
 
-rm ./Assets/gmcs.rsp
-rm ./Assets/MiyamasuTestRunner/Editor/Timestamp.cs
+
+# rm ./Assets/gmcs.rsp
+# rm ./Assets/MiyamasuTestRunner/Editor/Timestamp.cs
