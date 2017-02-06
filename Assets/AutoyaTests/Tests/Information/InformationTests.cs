1fd66885a16a94179952f01d395890e2925b534c
sassembla <toru.inoue@kissaki.tv>
2017-02-06 14:42:03 +0900

    GUIが出るようになってきた。

--- Assets/AutoyaTests/Tests/Information/InformationTests.cs
+++ Assets/AutoyaTests/Tests/Information/InformationTests.cs
@@ -8,6 +8,7 @@ using System.Xml.Linq;
 using System.IO;
 using System.Text;
 using System.Linq;
+using UnityEngine.UI;
 
 public class InformationTests : MiyamasuTestRunner {
 	[MTest] public void ParseSmallMarkdown () {
@@ -103,7 +104,7 @@ hard break will appear without <br />.
 		// 取れそうな戦略：
 		/*
 			次のようなhtmlが手に入るので、
-			hX, p, img, ul, li, aタグを見て、それぞれを「行単位の要素」に変形、uGUIの要素に変形できれば良さそう。
+			hX, p, img, ul, li, aとかのタグを見て、それぞれを「行単位の要素」に変形、uGUIの要素に変形できれば良さそう。
 			tokenizer書くか。
 
 			・tokenizerでいろんなコンポーネントに分解
@@ -112,47 +113,161 @@ hard break will appear without <br />.
 			
 			で、これらの機能は、「N行目から」みたいな指定で描画できるようにしたい。
 		*/
+		RunOnMainThread(
+			() => {
+				var tokenizer = new Tokenizer(text);
 
-        var tokenizer = new Tokenizer(text);
-        for (var i = 0; i < tokenizer.ComponentCount(); i++) {
-			var component = tokenizer.GetComponentAt(i);
-			// これがuGUIのコンポーネントになってる
-		}
+				// とりあえず全体を生成
+				tokenizer.Materialize();
 
-		/*
-			位置を指定して書き出すことができる(一番上がずれる = cursorか。)
-		*/
-		for (var i = 1; i < tokenizer.ComponentCount(); i++) {
-			var component = tokenizer.GetComponentAt(i);
-			// これがuGUIのコンポーネントになってる
-		}
+				// var childlen = obj.transform.GetChildlen();
 
+				// for (var i = 0; i < tokenizer.ComponentCount(); i++) {
+				// 	tokenizer.Materialize(i);
+				// 	// これがuGUIのコンポーネントになってる
+				// }
 
+				/*
+					位置を指定して書き出すことができる(一番上がずれる = cursorか。)
+				*/
+				// for (var i = 1; i < tokenizer.ComponentCount(); i++) {
+				// 	var component = tokenizer.GetComponentAt(i);
+				// 	// これがuGUIのコンポーネントになってる
+				// }
 
+				// GameObject.DestroyImmediate(obj);// とりあえず消す
+			}
+		);
 	}
 	/**
 		parse hX, p, img, ul, ol, li, a tags then returns GameObject for GUI.
 	*/
 	public class Tokenizer {
-		private readonly string source;
-		public Tokenizer (string source) {
-			this.source = source;
-			
-			var root = new TagPoint(0, Tag.ROOT);
-			Tokenize(root, source);
+		public class VirtualGameObject {
+			public GameObject _gameObject;
 
-			// Debug.LogError("root count:" + root.tagContents.Count);
-			// foreach (var t in root.tagContents) {
-			// 	Debug.LogError("tag:" + t.tag + " content:" + t.Stringify());
-			// }
+			public readonly Tag tag;
+			
+			private VirtualTransform _transform;
+			
+			public VirtualTransform transform {
+                get {
+					return _transform;
+				}
+            }
+
+			public Dictionary<string, string> kv = new Dictionary<string, string>();
+			public string content = string.Empty;
+
+			public VirtualGameObject (Tag tag) {
+				this.tag = tag;
+				this._transform = new VirtualTransform(this);
+			}
+
+			/**
+				return generated game object.
+			*/
+			public GameObject Materialize (Tag tag, GameObject parent) {
+				Debug.LogError("Materialize tag:" + tag);
+				this._gameObject = new GameObject(tag.ToString());// オブジェクトプールがあるといいのでは的な。
+				this._gameObject.transform.SetParent(parent.transform);
+
+				switch (tag) {
+					case Tag.ROOT: {
+						var childlen = this.transform.GetChildlen();
+						foreach (var child in childlen) {
+							child.Materialize(child.tag, this._gameObject);
+						}
+						break;
+					}
+
+					case Tag.P:
+					case Tag.H1:
+					case Tag.H2:
+					case Tag.H3:
+					case Tag.H4:
+					case Tag.H5: {
+						{
+							GameObject tDummyObj = GameObject.Find("dummy");
+								
+							Text tDummy;
+							if (tDummyObj == null) {
+								tDummyObj = new GameObject("dummy");
+								tDummy = tDummyObj.AddComponent<Text>();
+							} else {
+								tDummy = tDummyObj.GetComponent<Text>();
+							}
+
+							var generator = new TextGenerator();
+							var v2 = tDummy.GetComponent<RectTransform>().sizeDelta;
+							generator.Populate(content, tDummy.GetGenerationSettings(v2));
+							foreach(var l in generator.lines){
+								Debug.Log("ch index:" + l.startCharIdx);
+							}
+						}
+
+						// 親 -> 子、とだんだん幅を狭めていく感じでviewを実装。
+						// 高さは計算後に全部並べる感じか。結局遅延させられなさそう？
+						// 画面外になったら書かない、とかか。上下で超過した最大一個をdisableして、それ以降はskipとか。
+						// 仮でGameObjectを一つ持って、折り返しを計測するのに使える感じ。
+						this._gameObject.AddComponent<CanvasRenderer>();
+						var t = this._gameObject.AddComponent<Text>();
+						t.text = content;
+
+						var childlen = this.transform.GetChildlen();
+						foreach (var child in childlen) {
+							child.Materialize(child.tag, this._gameObject);
+						}
+						break;
+					}
+					default: {
+						// ここで、仮のUIパーツをいっぱい作って、いざ表示範囲指定の時に、型にあてていけばいいか。
+						// とりあえず組み上げ出してみよう。Pはなんかテキスト。中にリンク(a)が入ったりする。
+						break;
+					}
+				}
+
+				return this._gameObject;
+			}
 		}
 
-		private void Tokenize (TagPoint p, string data) {
+		public class VirtualTransform {
+			/*
+				このへんに、posとかwidthとか持てば良さそう。
+				Rootにコンテンツ全体のサイズを0,0,totalW,totalH,という値を持つような感じで保持して、
+				それ以下のコンテンツは下の最大値を持つ、とかしとけばよさげ。二重リストも存在しないし。
+			*/
+			private readonly VirtualGameObject gameObject;
+			private List<VirtualGameObject> _childlen = new List<VirtualGameObject>();
+			public VirtualTransform (VirtualGameObject gameObject) {
+				this.gameObject = gameObject;
+			}
+
+			public void SetParent (VirtualTransform t) {
+				t._childlen.Add(this.gameObject);
+			}
+
+			public List<VirtualGameObject> GetChildlen () {
+				return _childlen;
+			}
+		}
+
+		private readonly VirtualGameObject rootObject;
+
+		public Tokenizer (string source) {
+			var root = new TagPoint(0, Tag.ROOT);
+			rootObject = Tokenize(root, source);
+		}
+
+		public GameObject Materialize () {
+			return rootObject.Materialize(Tag.ROOT, new GameObject("sample"));
+		}
+
+		private VirtualGameObject Tokenize (TagPoint p, string data) {
 			var lines = data.Split('\n');
 			
 			var index = 0;
 
-			// 大外のline読み
 			while (true) {
 				if (lines.Length <= index) {
 					break;
@@ -166,37 +281,44 @@ hard break will appear without <br />.
 
 				// Debug.LogError("readLine:" + readLine);
 				
-				var foundStartTagPoint = FindStartTag(lines, index);
-				if (foundStartTagPoint.tag == Tag.NO_TAG_FOUND || foundStartTagPoint.tag == Tag.UNKNOWN) {
-					// detect single closed tag. e,g, <tag something />.
-					var foundSingleTagPoint = FindSingleTag(lines, index);
+				var foundStartTagPointAndAttr = FindStartTag(lines, index);
 
-					if (foundSingleTagPoint.tag != Tag.NO_TAG_FOUND && foundSingleTagPoint.tag != Tag.UNKNOWN) {
-						// closed tag found. add contents.
-						// p.tagContents.Add(new TagContent(foundSingleTagPoint.tag, foundSingleTagPoint.kv));	
+				/*
+					no <tag> found.
+				*/
+				if (foundStartTagPointAndAttr.tagPoint.tag == Tag.NO_TAG_FOUND || foundStartTagPointAndAttr.tagPoint.tag == Tag.UNKNOWN) {
+					// detect single closed tag. e,g, <tag something />.
+					var foundSingleTagPointWithAttr = FindSingleTag(lines, index);
+
+					if (foundSingleTagPointWithAttr.tagPoint.tag != Tag.NO_TAG_FOUND && foundSingleTagPointWithAttr.tagPoint.tag != Tag.UNKNOWN) {
+						// closed <tag /> found. add this new closed tag to parent tag.
+						AddChildContentToParent(p, foundSingleTagPointWithAttr.tagPoint, foundSingleTagPointWithAttr.attrs);
 					} else {
-						// not tag contained in this line. this line is just contents only. add to parent tag.
-						var content = readLine;
-						Debug.LogError("tag:" + p.tag + " content:" + content);
-						// p.tagContents.Add(new TagContent(p.tag, content));	
+						// not tag contained in this line. this line is just contents of parent tag.
+						AddContentToParent(p, readLine);
 					}
 					
 					index++;
 					continue;
 				}
 				
-				// foundStartTagPointがあるので、要素の開始が確定できた。最上位からの見出し的な要素を保持できるはず。
+				// tag opening found.
 				
 				while (true) {
-					if (lines.Length <= index) {// 閉じタグが見つからなかった
+					if (lines.Length <= index) {// 閉じタグが見つからなかったのでたぶんparseException.
 						break;
 					}
 
 					readLine = lines[index];
 					
 					// find end of current tag.
-					var foundEndTagPoint = FindEndTag(foundStartTagPoint, lines, index);
-					if (foundEndTagPoint.tag != Tag.NO_TAG_FOUND && foundStartTagPoint.tag != Tag.UNKNOWN) {
+					var foundEndTagPointAndContent = FindEndTag(foundStartTagPointAndAttr.tagPoint, lines, index);
+					if (foundEndTagPointAndContent.tagPoint.tag != Tag.NO_TAG_FOUND && foundEndTagPointAndContent.tagPoint.tag != Tag.UNKNOWN) {
+						// close tag found. set attr to this closed tag.
+						AddChildContentToParent(p, foundStartTagPointAndAttr.tagPoint, foundStartTagPointAndAttr.attrs);
+
+						// content exists. parse recursively.
+						Tokenize(foundEndTagPointAndContent.tagPoint, foundEndTagPointAndContent.content);
 						break;
 					}
 
@@ -207,6 +329,18 @@ hard break will appear without <br />.
 				// p.tagContents.AddRange(foundStartTagPoint.tagContents);
 				index++;
 			}
+
+			return p.gameObject;
+		}
+
+		private void AddChildContentToParent (TagPoint parent, TagPoint child, Dictionary<string, string> kv) {
+			var parentObj = parent.gameObject;
+			child.gameObject.transform.SetParent(parentObj.transform);
+			child.gameObject.kv = kv;
+		}
+		
+		private void AddContentToParent (TagPoint p, string content) {
+			p.gameObject.content = content;
 		}
 
 		public enum Tag {
@@ -226,54 +360,61 @@ hard break will appear without <br />.
 			A
 		}
 
-		public struct TagPoint {
+		public class TagPoint {
+			public readonly VirtualGameObject gameObject;
+
 			public readonly int lineIndex;
 			public readonly Tag tag;
+			
 			public TagPoint (int lineIndex, Tag tag) {
+				if (tag == Tag.NO_TAG_FOUND || tag == Tag.UNKNOWN) {
+					return;
+				}
+
 				this.lineIndex = lineIndex;
 				this.tag = tag;
+				this.gameObject = new VirtualGameObject(tag);	
+			}
+
+			public GameObject Materialize (GameObject parent) {
+				return this.gameObject.Materialize(tag, parent);
 			}
 		}
 
-		public struct TagContent {
-			public readonly Tag tag;
-			public readonly Dictionary<string, string> kv;
+
+		private struct TagPointAndAttr {
+			public readonly TagPoint tagPoint;
+			public readonly Dictionary<string, string> attrs;
+			
+			public TagPointAndAttr (TagPoint tagPoint, Dictionary<string, string> attrs) {
+				this.tagPoint = tagPoint;
+				this.attrs = attrs;
+			}
+			public TagPointAndAttr (TagPoint tagPoint) {
+				this.tagPoint = tagPoint;
+				this.attrs = new Dictionary<string, string>();
+			}
+		}
+
+		private struct TagPointAndContent {
+			public readonly TagPoint tagPoint;
 			public readonly string content;
-			public TagContent (Tag tag, string content) {
-				this.tag = tag;
-				this.kv = new Dictionary<string, string>();
+			
+			public TagPointAndContent (TagPoint tagPoint, string content) {
+				this.tagPoint = tagPoint;
 				this.content = content;
 			}
-			public TagContent (Tag tag, Dictionary<string, string> kv) {
-				this.tag = tag;
-				this.kv = kv;
+			public TagPointAndContent (TagPoint tagPoint) {
+				this.tagPoint = tagPoint;
 				this.content = string.Empty;
 			}
-			public TagContent (Tag tag, Dictionary<string, string> kv, string content) {
-				this.tag = tag;
-				this.kv = kv;
-				this.content = content;
-			}
-			public string Stringify () {
-				var result = string.Empty;
-				
-				if (0 < kv.Count) {
-					var kvArray = kv.Select(i => "key:" + i.Key + " val:" + i.Value).ToArray();
-					result = string.Join(", ", kvArray);
-				}
-
-				if (!string.IsNullOrEmpty(content)) {
-					if (!string.IsNullOrEmpty(result)) {
-						result += "\n"; 
-					}
-					result += content;
-				}
-
-				return result;
-			}
 		}
 
-		private TagPoint FindStartTag (string[] lines, int lineIndex) {
+
+		/**
+			find tag if exists.
+		*/
+		private TagPointAndAttr FindStartTag (string[] lines, int lineIndex) {
 			var line = lines[lineIndex];
 
 			// find <X>something...
@@ -281,13 +422,13 @@ hard break will appear without <br />.
 				var closeIndex = line.IndexOf(">");
 
 				if (closeIndex == -1) {
-					return new TagPoint(lineIndex, Tag.NO_TAG_FOUND);
+					return new TagPointAndAttr(new TagPoint(lineIndex, Tag.NO_TAG_FOUND));
 				}
 
 				// check found tag end has closed tag mark or not.
 				if (line[closeIndex-1] == '/') {
 					// closed tag detected.
-					return new TagPoint(lineIndex, Tag.NO_TAG_FOUND);
+					return new TagPointAndAttr(new TagPoint(lineIndex, Tag.NO_TAG_FOUND));
 				}
 
 				var tagName = string.Empty;
@@ -295,7 +436,7 @@ hard break will appear without <br />.
 
 				// not closed tag. contains attr or not.
 				if (line[closeIndex-1] == '"') {// <tag something="else">
-					var contents = line.Substring(1 - 1).Split(' ');
+					var contents = line.Substring(1, closeIndex - 1).Split(' ');
 					tagName = contents[0];
 					for (var i = 1; i < contents.Length; i++) {
 						var kv = contents[i].Split(new char[]{'='}, 2);
@@ -309,56 +450,23 @@ hard break will appear without <br />.
 
 						kvDict[key] = val;
 					}
-					var kvs = kvDict.Select(i => i.Key + " " + i.Value).ToArray();
-					Debug.LogError("tag:" + tagName + " contains kv:" + string.Join(", ", kvs));
-					
+					// var kvs = kvDict.Select(i => i.Key + " " + i.Value).ToArray();
+					// Debug.LogError("tag:" + tagName + " contains kv:" + string.Join(", ", kvs));
 				} else {
 					tagName = line.Substring(1, closeIndex - 1);
 				}
-					
-				switch (tagName) {
-					case "h1": {
-						return new TagPoint(lineIndex, Tag.H1);
-					}
-					case "h2": {
-						return new TagPoint(lineIndex, Tag.H2);
-					}
-					case "h3": {
-						return new TagPoint(lineIndex, Tag.H3);
-					}
-					case "h4": {
-						return new TagPoint(lineIndex, Tag.H4);
-					}
-					case "h5": {
-						return new TagPoint(lineIndex, Tag.H5);
-					}
-					case "p": {
-						return new TagPoint(lineIndex, Tag.P);
-					}
-					case "img": {
-						return new TagPoint(lineIndex, Tag.IMG);
-					}
-					case "ul": {
-						return new TagPoint(lineIndex, Tag.UL);
-					}
-					case "ol": {
-						return new TagPoint(lineIndex, Tag.OL);
-					}
-					case "li": {
-						return new TagPoint(lineIndex, Tag.LI);
-					}
-					case "a": {
-						return new TagPoint(lineIndex, Tag.A);
-					}
-					default: {
-						return new TagPoint(lineIndex, Tag.UNKNOWN);
-					}
+				
+				try {
+					var tagEnum = (Tag)Enum.Parse(typeof(Tag), tagName, true);
+					return new TagPointAndAttr(new TagPoint(lineIndex, tagEnum), kvDict);
+				} catch (Exception e) {
+					return new TagPointAndAttr(new TagPoint(lineIndex, Tag.UNKNOWN), kvDict);
 				}
 			}
-			return new TagPoint(lineIndex, Tag.NO_TAG_FOUND);
+			return new TagPointAndAttr(new TagPoint(lineIndex, Tag.NO_TAG_FOUND));
 		}
 
-		private TagPoint FindSingleTag (string[] lines, int lineIndex) {
+		private TagPointAndAttr FindSingleTag (string[] lines, int lineIndex) {
 			var line = lines[lineIndex];
 
 			// find <X>something...
@@ -366,13 +474,13 @@ hard break will appear without <br />.
 				var closeIndex = line.IndexOf(" />");
 
 				if (closeIndex == -1) {
-					return new TagPoint(lineIndex, Tag.NO_TAG_FOUND);
+					return new TagPointAndAttr(new TagPoint(lineIndex, Tag.NO_TAG_FOUND));
 				}
 
-				var contents = line.Substring(1 - 1).Split(' ');
+				var contents = line.Substring(1, closeIndex - 1).Split(' ');
 
 				if (contents.Length == 0) {
-					return new TagPoint(lineIndex, Tag.NO_TAG_FOUND);
+					return new TagPointAndAttr(new TagPoint(lineIndex, Tag.NO_TAG_FOUND));
 				}
 				
 				var tagName = contents[0];
@@ -390,50 +498,20 @@ hard break will appear without <br />.
 
 					kvDict[key] = val;
 				}
+				// var kvs = kvDict.Select(i => i.Key + " " + i.Value).ToArray();
+				// Debug.LogError("tag:" + tagName + " contains kv:" + string.Join(", ", kvs));
 				
-				switch (tagName) {
-					case "h1": {
-						return new TagPoint(lineIndex, Tag.H1);
-					}
-					case "h2": {
-						return new TagPoint(lineIndex, Tag.H2);
-					}
-					case "h3": {
-						return new TagPoint(lineIndex, Tag.H3);
-					}
-					case "h4": {
-						return new TagPoint(lineIndex, Tag.H4);
-					}
-					case "h5": {
-						return new TagPoint(lineIndex, Tag.H5);
-					}
-					case "p": {
-						return new TagPoint(lineIndex, Tag.P);
-					}
-					case "img": {
-						return new TagPoint(lineIndex, Tag.IMG);
-					}
-					case "ul": {
-						return new TagPoint(lineIndex, Tag.UL);
-					}
-					case "ol": {
-						return new TagPoint(lineIndex, Tag.OL);
-					}
-					case "li": {
-						return new TagPoint(lineIndex, Tag.LI);
-					}
-					case "a": {
-						return new TagPoint(lineIndex, Tag.A);
-					}
-					default: {
-						return new TagPoint(lineIndex, Tag.UNKNOWN);
-					}
+				try {
+					var tagEnum = (Tag)Enum.Parse(typeof(Tag), tagName, true);
+					return new TagPointAndAttr(new TagPoint(lineIndex, tagEnum), kvDict);
+				} catch (Exception e) {
+					return new TagPointAndAttr(new TagPoint(lineIndex, Tag.UNKNOWN), kvDict);
 				}
 			}
-			return new TagPoint(lineIndex, Tag.NO_TAG_FOUND);
+			return new TagPointAndAttr(new TagPoint(lineIndex, Tag.NO_TAG_FOUND));
 		}
 
-		private TagPoint FindEndTag (TagPoint p, string[] lines, int lineIndex) {
+		private TagPointAndContent FindEndTag (TagPoint p, string[] lines, int lineIndex) {
 			var line = lines[lineIndex];
 			var endTag = "</" + p.tag.ToString().ToLower() + ">";
 
@@ -447,7 +525,7 @@ hard break will appear without <br />.
 			
 			if (endTagIndex == -1) {
 				// no end tag contained.
-				return new TagPoint(lineIndex, Tag.NO_TAG_FOUND);
+				return new TagPointAndContent(new TagPoint(lineIndex, Tag.NO_TAG_FOUND));
 			}
 			
 			var contentsStrLines = lines.Where((i,l) => p.lineIndex <= l && l <= lineIndex).ToArray();
@@ -458,17 +536,10 @@ hard break will appear without <br />.
 			
 			var contentsStr = string.Join("\n", contentsStrLines);
 
-			// tokenize recursive.
-			Tokenize(p, contentsStr);
-
-			return p;
+			return new TagPointAndContent(p, contentsStr);
 		}
 
-		public int ComponentCount () {
-			return -1;
-		}
-
-		public GameObject GetComponentAt (int index) {
+		public GameObject Materialize (int index) {
 			return null;
 		}
 	}
