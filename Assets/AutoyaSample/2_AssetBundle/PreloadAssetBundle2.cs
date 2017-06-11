b6918c662db83b648cdc094150431e7505467ba0
sassembla <toru.inoue@kissaki.tv>
2017-06-12 02:08:06 +0900

    add preloading assetBundle sample: download preloadList from web.

--- Assets/AutoyaSample/2_AssetBundle/PreloadAssetBundle2.cs
+++ Assets/AutoyaSample/2_AssetBundle/PreloadAssetBundle2.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using AutoyaFramework;
 using AutoyaFramework.AssetBundles;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PreloadAssetBundle : MonoBehaviour {
+public class PreloadAssetBundle2 : MonoBehaviour {
 
 	// Use this for initialization
 	IEnumerator Start () {
@@ -15,9 +16,9 @@ public class PreloadAssetBundle : MonoBehaviour {
 			the word "preload" in this sample means "download assetBundles before use."
 			preloaded assetBundles are stored in storage cache. no difference between preloaded and downloaded assetBundles.
 
-			case1:generate preloadList from assetBundleList, then get described assetBundles.
+			case2:get preloadList from web, then get described assetBundles.
 		 */
-
+		
 		// Autoya.AssetBundle_DiscardAssetBundleList();
 
 		// download assetBundleList if assetBundleList is not stored.
@@ -25,7 +26,7 @@ public class PreloadAssetBundle : MonoBehaviour {
 
 		if (!isListStored) {
 			// store assetBundleList in file storage.
-
+			
 			var done = false;
 			Autoya.AssetBundle_DownloadAssetBundleList(
 				"https://raw.githubusercontent.com/sassembla/Autoya/master/AssetBundles/StandaloneOSXIntel64/1.0.0/AssetBundles.StandaloneOSXIntel64_1_0_0.json",
@@ -41,34 +42,46 @@ public class PreloadAssetBundle : MonoBehaviour {
 				yield return null;
 			}
 		}
+		
 
 		/*
-			let's preload specific assetBundle into storage.
-		*/
+			get preloadList from web.
+			the base filePath settings is located at below.
+				https://github.com/sassembla/Autoya/blob/f020e02d707781f80e70c91a3dfd943b95cda25c/Assets/Autoya/Settings/AssetBundlesSettings.cs
 
-		// get stored assetBundleList.
-		var storedList = Autoya.AssetBundle_AssetBundleList();
-		
-		// create sample preloadList which contains all assetBundle names in assetBundleList.
-		var assetBundleNames = storedList.assetBundles.Select(abInfo => abInfo.bundleName).ToArray();
-		var newPreloadList = new PreloadList("samplePreloadList", assetBundleNames);
+			this preloadList contains 1 assetBundleName, "bundlename", contains 1 asset, "textureName.png"
 
+			note that:
+				this feature requires the condition:"assetBundleList is stored." for getting assetBundleInfo. (crc, hash, and dependencies.)
+		 */
+
+		var preloadListPath = "1.0.0/sample.preloadList.json";
+		// this will become "https://dl.dropboxusercontent.com/u/36583594/outsource/Autoya/AssetBundle/Mac/1.0.0/sample.preloadList.json".
+
+
+		// download preloadList from web then preload described assetBundles.
 		Autoya.AssetBundle_Preload(
-			newPreloadList,
+			preloadListPath,
 			progress => {
 				Debug.Log("progress:" + progress);
 			},
 			() => {
-				Debug.Log("preloading all listed assetBundles is finished.");
+				Debug.Log("preloading 1 listed assetBundles is finished.");
 				
 				// then, you can use these assetBundles immediately. without any downloading.
-				Autoya.AssetBundle_LoadAsset<GameObject>(
-					"Assets/AutoyaTests/RuntimeData/AssetBundles/TestResources/textureName1.prefab",
-					(assetName, prefab) => {
-						Debug.Log("asset:" + assetName + " is successfully loaded as:" + prefab);
+				Autoya.AssetBundle_LoadAsset<Texture2D>(
+					"Assets/AutoyaTests/RuntimeData/AssetBundles/TestResources/textureName.png",
+					(assetName, image) => {
+						Debug.Log("asset:" + assetName + " is successfully loaded as:" + image);
 
-						// instantiate asset.
-						Instantiate(prefab);
+						// create gameObject, then set tex to it as sprite.
+						var gameObj = new GameObject("createdGameObject");
+						var imageComponent = gameObj.AddComponent<Image>();
+						imageComponent.sprite = Sprite.Create(image, new Rect(0.0f, 0.0f, image.width, image.height), new Vector2(0.5f, 0.5f), 100.0f);
+
+						// find uGUI canvas then set. 
+						var canvas = GameObject.Find("Canvas");
+						gameObj.transform.SetParent(canvas.transform, false);
 					},
 					(assetName, err, reason, status) => {
 						Debug.LogError("failed to load assetName:" + assetName + " err:" + err + " reason:" + reason);
