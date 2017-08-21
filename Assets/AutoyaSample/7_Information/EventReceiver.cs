cb0e6a0f9745e2abe5c581b01bc1229203c9dfe7
Toru <toru.inoue@kissaki.tv>
2017-08-21 19:38:03 +0900

    remove unnecessary log.

--- Assets/AutoyaSample/7_Information/EventReceiver.cs
+++ Assets/AutoyaSample/7_Information/EventReceiver.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 
 public class EventReceiver : MonoBehaviour, IUUebViewEventHandler {
 
-	 // Use this for initialization
+     // Use this for initialization
     void Start () {
-		var scrollView = GameObject.Find("Content");
-		
-		var url = "resources://InformationSampleRes/test.html";
-		var view = UUebViewCore.GenerateSingleViewFromUrl(this.gameObject, url, scrollView.GetComponent<RectTransform>().sizeDelta);
-		view.transform.SetParent(scrollView.transform, false);
-	}
+        var scrollView = GameObject.Find("Content");
+        
+        var url = "resources://InformationSampleRes/test.html";
+        var view = UUebViewCore.GenerateSingleViewFromUrl(this.gameObject, url, scrollView.GetComponent<RectTransform>().sizeDelta);
+        view.transform.SetParent(scrollView.transform, false);
+    }
 
     void IUUebViewEventHandler.OnElementLongTapped (ContentType type, string param, string id) {
         throw new System.NotImplementedException();
-		// まだ着火されない。
+        // まだ着火されない。
     }
 
     void IUUebViewEventHandler.OnElementTapped(ContentType type, GameObject element, string param, string id) {
@@ -24,18 +24,17 @@ public class EventReceiver : MonoBehaviour, IUUebViewEventHandler {
     }
 
     void IUUebViewEventHandler.OnLoaded () {
-		// ロードが終わったタイミングでビュー高さが取得できるんだけど、ここをスマートに反映させる方法を考え中。
+        // ロードが終わったタイミングでビュー高さが取得できるんだけど、ここをスマートに反映させる方法を考え中。
         var content = GameObject.Find("Content");
-		var scrollViewContentRectTrans = content.GetComponent<RectTransform>();
-		
-		foreach (Transform trans in content.transform) {
-			var childHeight = trans.GetComponent<RectTransform>().sizeDelta.y;
-			Debug.LogError("childHeight:" + childHeight);
-			scrollViewContentRectTrans.sizeDelta = new Vector2(scrollViewContentRectTrans.sizeDelta.x, childHeight);
-			break;
-		}
+        var scrollViewContentRectTrans = content.GetComponent<RectTransform>();
+        
+        foreach (Transform trans in content.transform) {
+            var childHeight = trans.GetComponent<RectTransform>().sizeDelta.y;
+            scrollViewContentRectTrans.sizeDelta = new Vector2(scrollViewContentRectTrans.sizeDelta.x, childHeight);
+            break;
+        }
 
-		Debug.Log("loaded.");
+        Debug.Log("loaded.");
     }
 
     void IUUebViewEventHandler.OnLoadFailed (ContentType type, int code, string reason) {
@@ -51,7 +50,7 @@ public class EventReceiver : MonoBehaviour, IUUebViewEventHandler {
     }
 
     void IUUebViewEventHandler.OnUpdated () {
-		Debug.Log("updated.");
+        Debug.Log("updated.");
     }
 
    
