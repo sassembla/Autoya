4a844ebb91c9184450c64288838a019e8881c4d5
Toru <toru.inoue@kissaki.tv>
2017-09-01 10:12:22 +0900

    refacotoring AutoyaStatus

--- Assets/Autoya/Backyard/AuthenticationImplementation.cs
+++ Assets/Autoya/Backyard/AuthenticationImplementation.cs
@@ -10,25 +10,6 @@ using System.Collections;
 	authentication impl.
 */
 namespace AutoyaFramework {
-
-	/**
-		struct for represents Autoya's specific status.
-		
-		if inMaintenance == true, server is in maintenance mode. == returns http code for maintenance.
-			see OverridePoint.cs "IsUnderMaintenance" method to change this behaviour.
-
-		if isAuthFailed == true, server returns 401.
-			see OverridePoint.cs "IsUnauthorized" method to change this behaviour.
-	*/
-	public struct AutoyaStatus {
-		public readonly bool inMaintenance;
-		public readonly bool isAuthFailed;
-		public AutoyaStatus (bool inMaintenance, bool isAuthFailed, bool userValidateFailed=false) {
-			this.inMaintenance = inMaintenance;
-			this.isAuthFailed = isAuthFailed;
-		}
-	}
-
 	public partial class Autoya {
 		private AuthRouter _autoyaAuthRouter;
 
