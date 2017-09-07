188b666d36ab4e9609fba599e82e60811d13e80e
Toru <toru.inoue@kissaki.tv>
2017-09-08 06:18:03 +0900

    refactor: update package making feature.

--- Assets/AutoyaTests/RuntimeData/AssetBundles/TestResources/Editor/Bundlizer.cs
+++ Assets/AutoyaTests/RuntimeData/AssetBundles/TestResources/Editor/Bundlizer.cs
@@ -23,18 +23,24 @@ using System.IO;
 
 	[MenuItem("Window/Autoya/Update UnityPackage")] public static void UnityPackage () {
 		var assetPaths = new List<string>();
-		var dirPaths = Directory.GetDirectories("Assets/Autoya");
-		
-		foreach (var dir in dirPaths) {
-			var files = Directory.GetFiles(dir);
-			foreach (var file in files) {
-				assetPaths.Add(file);
-			}
-		}
+		var frameworkPath = "Assets/Autoya";
+
+		CollectPathRecursive(frameworkPath, assetPaths);
 
 		AssetDatabase.ExportPackage(assetPaths.ToArray(), "Autoya.unitypackage", ExportPackageOptions.IncludeDependencies);
 	}
 
+	private static void CollectPathRecursive (string path, List<string> collectedPaths) {
+		var filePaths = Directory.GetFiles(path);
+		foreach (var filePath in filePaths) {
+			collectedPaths.Add(filePath);
+		}
+		
+		var modulePaths = Directory.GetDirectories(path);
+		foreach (var folderPath in modulePaths) {
+			CollectPathRecursive(folderPath, collectedPaths);
+		}
+	}
 
 	static Bundlizer () {
 		// create unitypackage if compiled.
