95373ad14ac941cef9b0969ce2c1f244c725fcaf
Toru <toru.inoue@kissaki.tv>
2017-09-20 18:56:02 +0900

    add dummy Apple/GooglePlayTangle files for compile platform code on CloudBuild.

---
+++ Assets/Plugins/UnityPurchasing/generated/AppleTangle.cs
@@ -0,0 +1,20 @@
+#if UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX || UNITY_TVOS
+// WARNING: Do not modify! Generated file.
+
+namespace UnityEngine.Purchasing.Security {
+    public class AppleTangle
+    {
+        private static byte[] data = System.Convert.FromBase64String("pSthpP3qUb9X5MM+l1GMGO3271YLiuJW4L6INtIJNadk38lF3eTfBomM4IrYi7GKs7y57768qbjv6Yupzc2U28rK1t+U2dXXldvKytbf2dsRGcso/envexWV+wlCQVnKd1wZ9pr5+4o4u5iKt7yzkDzyPE23u7u7nIqevLnvvrGpp/vKytbfmvnfyM7I29nO09nfmsnO287f19/UzsmUinOjyE/ntG/F5SFIn7kA7zX357dLv7q5OLu1uoo4u7C4OLu7ul4rE7PT3NPZ287T1dSa+8/O0tXI087Di901sg6aTXEWlprVygyFu4o2Dfl1vVbHgzkx6Zppgn4LBSD1sNFFkUbAiji7zIq0vLnvp7W7u0W+vrm4u7Unh0mR85KgckR0DwO0Y+SmbHGHBE7JIVRo3rVxw/WOYhiEQ8JF0XKKq7y5776wqbD7ysrW35rz1NmUi7yKtby576epu7tFvr+Kubu7RYqnrIquvLnvvrmpt/vKytbfmujV1c6PiIuOiomM4K23iY+KiIqDiIuOirKRvLu/v724u6yk0s7OysmAlZXNxfsSIkNrcNwmntGrahkBXqGQeaWy5Io4u6u8ue+nmr44u7KKOLu+ir68qbjv6Yupiqu8ue++sKmw+8rKjCP2l8INVzYhZknNIUjMaM2K9XuKOL4Biji5GRq5uLu4uLu4ire8s9bfmvPU2ZSLnIqevLnvvrGpp/vK/8Sl9tHqLPszfs7Ysao5+z2JMDvo39bT29TZ35rV1JrO0tPJmtnfyC8kwLYe/THhbqyNiXF+tfd0rtNrmtvU3prZ38jO09zT2dvO09XUmsrO09zT2dvO35rYw5rb1MOaytvIzuMdv7PGrfrsq6TOaQ0xmYH9GW/VkDzyPE23u7u/v7qK2IuxirO8ue9jjMV7Pe9jHSMDiPhBYm/LJMQb6BJmxJiPcJ9vY7Vs0W4YnpmrTRsWNck72nyh4bOVKAhC/vJK2oIkr0/ej5mv8a/jpwkuTUwmJHXqAHvi6s7S1cjTzsOLrIquvLnvvrmpt/vKlYo7ebyykby7v7+9uLiKOwygOwma1dyaztLfms7S39Sa28rK1tPZ25T6HE3998Wy5IqlvLnvp5m+ooqsD4AXTrW0uiixC5uslM5vhrdh2Kx62YnNTYC9luxRYLWbtGAAyaP1Dzi7uryzkDzyPE3Z3r+7ijtIipC8vLnvp7S+rL6ukWrT/S7Ms0RO0TcNoQcp+J6okH21pwz3JuTZcvE6rcrW35ro1dXOmvn7iqStt4qMio6IlprZ38jO09zT2dvO35rK1dbT2cPU3prZ1dTe087T1dTJmtXcms/J38Oa28nJz9ffyZrb2dnfys7b1NnfpT85P6Ejh/2NSBMh+jSWbgsqqGIxozNkQ/HWT70RmIq4UqKEQuqzacrW35r538jO09zT2dvO09XUmvvPOq6RatP9LsyzRE7RN5T6HE3998XY1t+ayc7b1N7byN6azt/I18ma255YUWsNymW1/1udcEvXwlddD62t82LMJYmu3xvNLnOXuLm7ursZOLu3vLOQPPI8Tbe7u7+/urk4u7u65oec3ZowidBNtzh1ZFEZlUPp0OHe6hAwb2BeRmqzvY0Kz8+b");
+        private static int[] order = new int[] { 15,26,8,36,24,13,10,46,19,14,11,21,51,28,22,50,22,29,51,46,35,29,33,46,48,50,36,50,38,51,45,39,52,36,58,54,55,54,38,54,41,54,46,57,58,45,59,51,58,57,54,57,55,58,55,59,58,59,59,59,60 };
+        private static int key = 186;
+
+        public static readonly bool IsPopulated = true;
+
+        public static byte[] Data() {
+        	if (IsPopulated == false)
+        		return null;
+            return Obfuscator.DeObfuscate(data, order, key);
+        }
+    }
+}
+#endif
