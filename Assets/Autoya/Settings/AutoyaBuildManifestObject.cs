b84b6fda52371eaaaf2ff7f7d42984c069cda6a6
Toru <toru.inoue@kissaki.tv>
2017-10-12 21:57:10 +0900

    refactor: move Autoya specific files to Settings folder.

--- Assets/Autoya/Settings/AutoyaBuildManifestObject.cs
+++ Assets/Autoya/Settings/AutoyaBuildManifestObject.cs
@@ -2,6 +2,14 @@ using System;
 using UnityEngine;
 
 namespace AutoyaFramework.AppManifest {
+     /**
+        Autoyaで使用する静的な設定パラメータに関する型情報。
+        Resourcesに保存される。
+
+        動的に書き換えることができない。
+        初期値を与えることができる。
+    */
+
     [Serializable] public class BuildManifestObject {
         [SerializeField] public string appVerion = "1.0.0";
         [SerializeField] public string buildNo;
