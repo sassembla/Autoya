2d5c99b869fee23bba538ac432fa0d916f6ea827
Toru <toru.inoue@kissaki.tv>
2017-10-13 17:28:48 +0900

    testing.

--- Assets/Autoya/Manifest/AppManifestStore.cs
+++ Assets/Autoya/Manifest/AppManifestStore.cs
@@ -205,7 +205,8 @@ namespace AutoyaFramework.AppManifest {
                     var cloudBuildManifestStr = Resources.Load<TextAsset>("UnityCloudBuildManifest.json").text;
                     try {
                         Debug.Log("cloudBuildManifestStr:" + cloudBuildManifestStr);
-                        var cloudBuildManifest = JsonUtility.FromJson<UnityEngine.CloudBuild.BuildManifestObject>(cloudBuildManifestStr);
+                        var cloudBuildManifest = JsonUtility.FromJson<CloudBuildManifest>(cloudBuildManifestStr);
+                        
                         try {
                             var cloudBuildManifestDict = cloudBuildManifest.GetType()
                                 .GetFields(BindingFlags.Instance | BindingFlags.Public).ToArray();
@@ -260,16 +261,41 @@ namespace AutoyaFramework.AppManifest {
     }
 }
 
-// namespace UnityEngine.CloudBuild {// dummy.
-//     public class BuildManifestObject : ScriptableObject {
-//         public string scmCommitId;
-//         public string scmBranch;
-//         public string buildNumber;
-//         public string buildStartTime;
-//         public string projectId;
-//         public string bundleId;
-//         public string unityVersion;
-//         public string xcodeVersion;
-//         public string cloudBuildTargetName;
-//     }
-// }
+[Serializable]
+public class CloudBuildManifest {
+    [SerializeField]
+    string scmCommitId;
+    public string ScmCommitId { get { return scmCommitId; } }
+
+    [SerializeField]
+    string scmBranch;
+    public string ScmBranch { get { return scmBranch; } }
+
+    [SerializeField]
+    string buildNumber;
+    public string BuildNumber { get { return buildNumber; } }
+
+    [SerializeField]
+    string buildStartTime;
+    public string BuildStartTime { get { return buildStartTime; } }
+
+    [SerializeField]
+    string projectId;
+    public string ProjectId { get { return projectId; } }
+
+    [SerializeField]
+    string bundleId;
+    public string BundleId { get { return bundleId; } }
+
+    [SerializeField]
+    string unityVersion;
+    public string UnityVersion { get { return unityVersion; } }
+
+    [SerializeField]
+    string xcodeVersion;
+    public string XCodeVersion { get { return xcodeVersion; } }
+
+    [SerializeField]
+    string cloudBuildTargetName;
+    public string CloudBuildTargetName { get { return cloudBuildTargetName; } }
+}
