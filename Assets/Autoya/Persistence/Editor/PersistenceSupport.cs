01d2c5bd8182646e0f4dc91e4354ba7017adb9ff
Toru <toru.inoue@kissaki.tv>
2018-02-25 23:37:22 +0900

    feature: PersistenceFile delete editorWindow supported.

---
+++ Assets/Autoya/Persistence/Editor/PersistenceSupport.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+/**
+	support features for control data storage for app.
+*/
+namespace AutoyaFramework.Persistence.Files
+{
+    public class PersistenceSupport
+    {
+        [MenuItem("Window/Autoya/Persistence/Delete Stored Persistent Folders")]
+        public static void DeleteFiles()
+        {
+            var persistencePath = Application.persistentDataPath;
+            var filePaths = Directory.GetDirectories(persistencePath);
+            if (filePaths.Any())
+            {
+                var window = EditorWindow.GetWindow<DeleteWindow>(typeof(DeleteWindow));
+                window.Init(filePaths.Select(p => new DirectoryInfo(p).Name).ToArray());
+                window.titleContent = new GUIContent("Delete Folders");
+                return;
+            }
+
+            Debug.Log("no persist file found.");
+        }
+    }
+
+    public class DeleteWindow : EditorWindow
+    {
+        private string[] dirNames;
+        private bool[] buttons;
+
+        private string basePath;
+        public void Init(string[] dirNames)
+        {
+            this.dirNames = dirNames;
+            this.buttons = new bool[dirNames.Length];
+            this.basePath = Application.persistentDataPath;
+        }
+
+        void OnGUI()
+        {
+            GUILayout.Label("Select folder(s) to delete.");
+
+            for (var i = 0; i < buttons.Length; i++)
+            {
+                using (new GUILayout.VerticalScope())
+                {
+                    using (new GUILayout.HorizontalScope())
+                    {
+                        var buttonName = dirNames[i];
+                        buttons[i] = GUILayout.Toggle(buttons[i], buttonName);
+                    }
+                }
+            }
+
+            if (GUILayout.Button("Delete Selected Folders"))
+            {
+                var deleteTargetPaths = new List<string>();
+                for (var i = 0; i < buttons.Length; i++)
+                {
+                    var button = buttons[i];
+                    if (button)
+                    {
+                        var folderName = dirNames[i];
+                        deleteTargetPaths.Add(Path.Combine(basePath, folderName));
+                    }
+                }
+
+                foreach (var path in deleteTargetPaths)
+                {
+                    Directory.Delete(path, true);
+                    Debug.Log("folder deleted:" + path);
+                }
+                this.Close();
+            }
+
+            using (new GUILayout.VerticalScope())
+            {
+                GUILayout.Label("note:these folders are located at persistentDataPath:");
+                GUILayout.TextField(basePath);
+                GUILayout.Label("the 'Unity' folder contains AssetBundle caches & Purchase record datas.");
+            }
+        }
+    }
+}
