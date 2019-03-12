a3d6ed59be208d0c59c8d2ee792f304c0d06bef0
sassembla <toru.inoue@kissaki.tv>
2019-03-13 05:22:42 +0900

    Added: AssetBundles module isolation.

---
+++ Assets/Editor/Packager/AssetBundles/AssetBundlesLoadSample.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AutoyaFramework.AssetBundles;
+using UnityEngine;
+
+public class AssetBundlesLoadSample : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        var LIST_DOWNLOAD_URL = "https://raw.githubusercontent.com/sassembla/Autoya/master/AssetBundles/main_assets/OSX/1.0.0/main_assets.json";
+        var ASSET_NAME = "Assets/AutoyaTests/RuntimeData/AssetBundles/MainResources/textureName.png";
+
+        // Download AssetBundle List from web.
+        var listDownloader = new AssetBundleListDownloader();
+
+        AssetBundleList list = null;
+        var cor = listDownloader.DownloadAssetBundleList(
+            LIST_DOWNLOAD_URL,
+            (url, newList) =>
+            {
+                list = newList;
+            },
+            (code, reason, status) =>
+            {
+                Debug.LogError("list download error, code:" + code + " reason:" + reason);
+            }
+        );
+
+        while (cor.MoveNext())
+        {
+            yield return null;
+        }
+
+
+        // Load Asset from your AssetBundle.
+        var loader = new AssetBundleLoader(
+            bundleName =>
+            {
+                var path = "https://raw.githubusercontent.com/sassembla/Autoya/master/AssetBundles/" +
+                    list.identity + "/" +
+                    list.target + "/" +
+                    list.version + "/";
+                return path;
+            }
+        );
+
+        // set list to AssetBundleLoader.
+        loader.UpdateAssetBundleList(list);
+
+        var loadCor = loader.LoadAsset<Texture2D>(
+            ASSET_NAME,
+            (name, tex) =>
+            {
+                Debug.Log("asset loaded! tex:" + tex);
+            },
+            (name, error, reason, status) =>
+            {
+                Debug.LogError("asset load failed. error:" + error + " reason:" + reason);
+            }
+        );
+
+        while (loadCor.MoveNext())
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Editor/Packager/AssetBundles/AssetBundlesPackager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class AssetBundlesPackager
+{
+    public static void UnityPackage()
+    {
+        var assetPaths = new List<string>();
+
+        var packagePath = "Assets/Autoya/AssetBundle";
+        CollectPathRecursive(packagePath, assetPaths);
+
+        assetPaths.Add("Assets/Autoya/Settings/AssetBundlesSettings.cs");
+        assetPaths.Add("Assets/Autoya/Backyard/BackyardSettings.cs");
+        assetPaths.Add("Assets/Autoya/AutoyaDependencies.cs");
+
+        AssetDatabase.ExportPackage(assetPaths.ToArray(), "Autoya.AssetBundles.unitypackage", ExportPackageOptions.IncludeDependencies);
+    }
+
+    private static void CollectPathRecursive(string path, List<string> collectedPaths)
+    {
+        var filePaths = Directory.GetFiles(path);
+        foreach (var filePath in filePaths)
+        {
+            collectedPaths.Add(filePath);
+        }
+
+        var modulePaths = Directory.GetDirectories(path);
+        foreach (var folderPath in modulePaths)
+        {
+            CollectPathRecursive(folderPath, collectedPaths);
+        }
+    }
+
+    static AssetBundlesPackager()
+    {
+        // create unitypackage if compiled.
+        if (!EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            UnityPackage();
+        }
+    }
+}
