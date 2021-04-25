efb61648c77328d56ae5d6191a6ed014d1e72ec9
sassembla <toru.inoue@kissaki.tv>
2021-04-25 19:24:30 +0900

    changed: SIWA build setting script adopt to Unity 2020.1.x.

--- Assets/Autoya/ThirdPartyAuthentication/SignInWithApple/Editor/BuildPostprocess_SignInWIthApple.cs
+++ Assets/Autoya/ThirdPartyAuthentication/SignInWithApple/Editor/BuildPostprocess_SignInWIthApple.cs
@@ -1,10 +1,8 @@
+#if UNITY_IOS
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.Callbacks;
-using System.Collections.Generic;
 using System.IO;
-
-#if UNITY_IOS
 using UnityEditor.iOS.Xcode;
 
 public static class BuildPostprocess_SignInWIthApple
@@ -12,47 +10,40 @@ public static class BuildPostprocess_SignInWIthApple
     [PostProcessBuild(999)]
     public static void OnPostProcessBuild(BuildTarget target, string path)
     {
-        if (target == BuildTarget.iOS)
+        // create PBXProject.
+        PBXProject proj = new PBXProject();
+        var xcodeProjPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+        proj.ReadFromFile(xcodeProjPath);
+
+        // enable SIWA.
+        var mainTargetGuid = proj.GetUnityMainTargetGuid();
+        var targetName = "Unity-iPhone";
+        var productName = proj.GetBuildPropertyForAnyConfig(mainTargetGuid, "PRODUCT_NAME_APP");
+        var entitlementsPath = targetName + "/" + Application.productName + ".entitlements";
+        proj.SetBuildProperty(mainTargetGuid, "CODE_SIGN_ENTITLEMENTS", entitlementsPath);
+
+        var frameworkGuid = proj.GetUnityFrameworkTargetGuid();
+        proj.AddFrameworkToProject(frameworkGuid, "AuthenticationServices.framework", true);
+
+        var entPath = path + "/" + entitlementsPath;
         {
-            // pbxプロジェクトファイルのパス
-            string xcodeProjPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
-
-            // プロジェクトファイルの読み込み
-            PBXProject proj = new PBXProject();
-            string projFile = File.ReadAllText(xcodeProjPath);
-            proj.ReadFromString(projFile);
-            var targetName = PBXProject.GetUnityTargetName();
-            var guid = proj.TargetGuidByName(targetName);
-
-            proj.AddCapability(guid, PBXCapabilityType.SignInWithApple);
-
-            proj.AddFrameworkToProject(guid, "AuthenticationServices.framework", true);
-
-            // SignInWithApple有効化
-            var productName = proj.GetBuildPropertyForAnyConfig(guid, "PRODUCT_NAME");
-            var entitlementsPath = targetName + "/" + productName + ".entitlements";
-            proj.SetBuildProperty(guid, "CODE_SIGN_ENTITLEMENTS", entitlementsPath);
-
-            var entPath = path + "/" + entitlementsPath;
+            var plist = new PlistDocument();
+            if (File.Exists(entPath))
             {
-                var plist = new PlistDocument();
-                if (File.Exists(entPath))
-                {
-                    plist.ReadFromFile(entPath);
-                }
-
-                var rootDict = plist.root;
-
-                var valueArray = new PlistElementArray();
-                valueArray.AddString("Default");
-                rootDict.values["com.apple.developer.applesignin"] = valueArray;
-
-                File.WriteAllText(entPath, plist.WriteToString());
+                plist.ReadFromFile(entPath);
             }
 
-            // 設定したデータを書き出す
-            proj.WriteToFile(xcodeProjPath);
+            var rootDict = plist.root;
+
+            var valueArray = new PlistElementArray();
+            valueArray.AddString("Default");
+            rootDict.values["com.apple.developer.applesignin"] = valueArray;
+
+            File.WriteAllText(entPath, plist.WriteToString());
         }
+
+        // writeout project.
+        proj.WriteToFile(xcodeProjPath);
     }
 }
 #endif
